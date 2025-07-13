0c1d17622f3ed27baa245af7362800af8cfe988c
lucinda279 <lucinda.279@gmail.com>
2025-07-13 14:19:44 +0700

    setup problem2 project structure

---
+++ src/problem2/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./libs/ui/button";
+
+function App() {
+  return (
+    <div>
+      <p>Hello world!</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
